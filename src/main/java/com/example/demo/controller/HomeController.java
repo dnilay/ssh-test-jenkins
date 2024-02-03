46a6a20f5f80d6268cb1a17b4b76bf7289aceeac
Nilay Das <mailnilaydas@gmail.com>
2024-02-03 11:29:12 +0530

    2nd commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+	@GetMapping
+	public ResponseEntity<?> status() throws UnknownHostException
+	{
+		return ResponseEntity.ok("status ok. ip: "+InetAddress.getLocalHost().toString());
+	}
 }
